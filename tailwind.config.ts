788419928b793e3beda0b4ed90dd2b1a8f49e1a2
Ibrohim Mubarok <befousmain@gmail.com>
2024-10-19 20:50:23 +0700

    Ganti setingan dikit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = {
+const config: Config = withMT({
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {
@@ -14,6 +17,8 @@ const config: Config = {
             },
         },
     },
-    plugins: [require('daisyui')],
-};
+    plugins: [
+        require('daisyui')
+    ],
+});
 export default config;
