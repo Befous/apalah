c6fa0fbf2b6127faa8f0b067ffe06184876f4a9c
Ibrohim Mubarok <befousmain@gmail.com>
2024-10-20 12:05:22 +0700

    Tambah fetch data buat mangadex

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -17,3 +17,48 @@ export async function fetchUsers(page: number, limit: number) {
         limit: limit
     }
 }
+
+export async function fetchPopularNewTitle() {
+    const date = new Date()
+    date.setMonth(date.getMonth() - 1)
+
+    const year = date.getFullYear()
+    const month = (date.getMonth() + 1).toString().padStart(2, '0')
+    const day = date.getDate().toString().padStart(2, '0')
+    const hours = '00'
+    const minutes = '00'
+    const seconds = '00'
+
+    const createdAtSince = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`
+
+    const response = await fetch(`https://api.mangadex.org/manga?includes[]=cover_art&includes[]=artist&includes[]=author&order[followedCount]=desc&contentRating[]=safe&contentRating[]=suggestive&contentRating[]=erotica&hasAvailableChapters=true&createdAtSince=${encodeURIComponent(createdAtSince)}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+    const data = await response.json()
+    
+    return data
+}
+
+export async function fetchCover(id_manga: string, id_cover: string) {
+    const response = await fetch(`https://api.mangadex.org/cover/${id_cover}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return `https://uploads.mangadex.org/covers/${id_manga}/${data.data.attributes.fileName}.512.jpg`
+}
