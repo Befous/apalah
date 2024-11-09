819d41d68c10b4f9f215afcfc02ce852ff5a06ee
Ibrohim Mubarok <befousmain@gmail.com>
2024-11-09 12:27:34 +0700

    Hapus force-dynamic ga tau gunanya apaan

--- src/app/api/users/route.tsx
+++ src/app/api/users/route.tsx
@@ -2,8 +2,6 @@ import { connectMongoDB } from "@/lib/mongodb"
 import User from "@/models/user"
 import { NextRequest, NextResponse } from "next/server"
 
-export const dynamic = 'force-dynamic'
-
 export async function GET(request: NextRequest) {
     try {
         await connectMongoDB()
